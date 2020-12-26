7d434801b4a1c10e6977960406cd6109b069b3cd
pavan kumar punna <punnapavankumar9@gmail.com>
2020-12-26 14:18:33 +0530

    secret key changed

--- web_blog/settings.py
+++ web_blog/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '095&#$5^lmhe509-g*n#cw&bbxqp43puopiu#b4xi2(dfamrgs'
+SECRET_KEY = os.environ.get('DJANGO_WEB_BLOG_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
