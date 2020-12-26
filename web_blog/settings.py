e510b9c897de69ceb660612a8863c93ef7a86d18
pavan kumar punna <punnapavankumar9@gmail.com>
2020-12-26 13:29:20 +0530

    added gunicorn django-heroku and Procfile

--- web_blog/settings.py
+++ web_blog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -163,3 +164,5 @@ AWS_S3_REGION_NAME = 'ap-south-1'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
+
+django_heroku.settings(locals())
