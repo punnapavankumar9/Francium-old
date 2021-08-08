d10681be1ce58729b4f108093bf8f2d4dfcbd13d
pavan kumar punna <67155391+punnapavankumar9@users.noreply.github.com>
2021-08-08 13:57:59 +0530

    authentication implemented

---
+++ accounts/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
+from django.db import models
+from django.forms import fields
+
+class UserSignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=255, required=True)
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2', )
+
+
+
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from .forms import UserSignUpForm
 
 # Create your views here.
+
+def signup_view(request):
+    if request.method == "POST":
+        form = UserSignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('core:index')
+    else:
+        form = UserSignUpForm()
+    return render(request, 'accounts/signup.html', {'form' : form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request = request, data = request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=raw_password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, "user login successfull")
+                return redirect('core:index')
+            else:
+                messages.error(request, "invalid username and password combination")
+        else:
+            messages.error(request, "invalid username and password combination")
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', context={'form' : form})
+
+def logout_view(request):
+    logout(request)
+    messages.info(request, "logged out successfull")
+    return redirect('core:index')
