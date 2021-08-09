297be1d2da3d815d60ecbf08e7d79e6b14361ebd
pavan kumar punna <67155391+punnapavankumar9@users.noreply.github.com>
2021-08-09 13:32:04 +0530

    added profile model and register to admin site

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import UserPofile
 # Register your models here.
+
+class UserProfileAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(UserPofile, UserProfileAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+#choices for models
+
+GenderChoices = (
+    ('N', 'None'),
+    ('M', 'Male'),
+    ('F', 'Female'),
+    ('O', 'Others'),
+)
 
 # Create your models here.
+class UserPofile(models.Model):
+    user = models.OneToOneField(User, related_name="profile", on_delete=models.CASCADE)
+    gender = models.CharField(max_length=20,null=True, blank=True, choices=GenderChoices,default=None)
+    profile_pic = models.ImageField(upload_to='profile_pics', default='profile_pics/default.png')
+
+    def __str__(self):
+        return f'{self.user.username}`s profile'
+
